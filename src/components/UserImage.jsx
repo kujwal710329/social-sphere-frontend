f111527bd8ddd09dff42c8ba193c042afcb3b3b7
Ujjwal Kumar <kujwal7103297@gmail.com>
2023-09-28 10:53:38 +0530

    updated baseurl for image

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -1,9 +1,10 @@
 import { Box } from "@mui/material";
+import BASE_URL from "BackendUrl";
 
 const UserImage = ({ image, size = "60px" }) => {
   return (
     <Box width={size} height={size}>
-      <img style={{ objectFit: "cover", borderRadius: "50%" }} width={size} height={size} alt="user" src={`/assets/${image}`} />
+      <img style={{ objectFit: "cover", borderRadius: "50%" }} width={size} height={size} alt="user" src={`${BASE_URL}/assets/${image}`} />
     </Box>
   );
 };
