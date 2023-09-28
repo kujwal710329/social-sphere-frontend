37e496d285420f46b4413baf68136003b43d6bbf
Ujjwal Kumar <kujwal7103297@gmail.com>
2023-09-28 10:03:39 +0530

    corrected baseUrl

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -68,7 +68,7 @@ const Form = () => {
   // };
 
   const login = async (e) => {
-    const loggedInResponse = await fetch(`{BASE_URL}/auth/login`, {
+    const loggedInResponse = await fetch(`${BASE_URL}/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
