a274e6e1747d82658a1607350c5e758a7238b8cf
Ujjwal Kumar <kujwal7103297@gmail.com>
2023-11-22 12:01:08 +0530

    hosted backend on railway

--- src/BackendUrl.jsx
+++ src/BackendUrl.jsx
@@ -1,2 +1,3 @@
-const BASE_URL = "https://social-sphere-backend-0ms7.onrender.com";
+// const BASE_URL = "https://social-sphere-backend-0ms7.onrender.com";
+const BASE_URL = "social-sphere-backend-production.up.railway.app";
 export default BASE_URL;
